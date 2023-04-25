a24ae3091f7f49a2e1fca1284e8d7121bb3b16b4
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-04-25 21:32:56 +0530

    Create debuging_errors_for_oddneven.py

---
+++ debuging_errors_for_oddneven.py
@@ -0,0 +1,14 @@
+#2. Debug the following program to check if the number is odd or even:
+#   num = input('enter number: ')
+#   if num/2 != 0:
+#      print("even")
+#   else:
+#      print(odd)
+
+
+
+num = int(input('enter number: '))
+if num%2==0:
+     print("even")
+else:
+     print("odd")
