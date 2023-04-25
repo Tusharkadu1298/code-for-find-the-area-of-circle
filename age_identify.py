cc32d53b0ca9eb3b4bc329795381bd2b7e2da0e9
Tushar Dattaguru Kadu <131634787+Tusharkadu1298@users.noreply.github.com>
2023-04-25 21:25:34 +0530

    Create age_identify.py

---
+++ age_identify.py
@@ -0,0 +1,6 @@
+#write a program where take age as an input from the user and identify if the person is eligible to vote or not.
+age = int(input("enter your age :-- "))
+if age >= 18:
+    print("you can vote ")
+else:
+    print("you are underage cant vote ")
